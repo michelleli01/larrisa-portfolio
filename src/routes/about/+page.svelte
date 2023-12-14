20d79cf0405fa43106aa1150fb07ad1a63193de3
Michelle Li <michelleyli123@gmail.com>
2023-12-14 11:40:14 -0500

    about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,87 @@
+<script>
+	import headshot from '$lib/assets/headshot.jpg';
+</script>
+
+<section
+	class="flex flex-col align-center flex max-w-[70%] gap-[4rem] mx-auto"
+>
+<div></div>
+	<div class="flex justify-between align-center gap-[1.25rem]">
+		<div class="max-w-[40rem]">
+			<div class="flex justify-center align-center gap-[1.25rem] max-h-[5rem] max-w-[15rem]">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="52"
+					height="79"
+					viewBox="0 0 52 79"
+					fill="none"
+					preserveAspectRatio="none"
+				>
+					<path
+						d="M42.2754 0.692627C47.5863 3.13984 49.4659 5.79849 51.1386 12.6926M4.91254 40.7019C3.43712 46.8366 4.33388 49.7974 10.9125 53.6926M0.912386 44.6926C-0.257397 49.7351 1.05601 50.8978 4.91239 51.6926M30.9122 78.2019C29.9331 78.2018 29.0501 61.1926 25.4122 54.6926C21.7743 48.1926 16.9164 37.2602 16.7534 36.1929C16.5904 35.1255 7.89127 22.702 10.9122 20.2019C13.9331 17.7017 19.4127 31.7019 20.9122 31.7019C22.4117 31.7019 14.8889 11.702 18.9122 11.2019C22.9354 10.7017 25.9011 30.1592 26.8889 30.202C27.8767 30.2449 21.4239 7.23844 25.4122 6.70186C29.4005 6.16528 31.4355 29.2016 32.4122 29.2019C33.3889 29.2021 33.4122 8.70186 36.8913 9.70188C40.3703 10.7019 37.3913 38.7019 40.4122 39.2019C43.4331 39.7018 45.8913 28.702 48.8913 30.702C51.8913 32.702 46.4331 42.1924 45.4122 43.6926C44.3913 45.1928 39.9331 53.2017 40.4122 56.7019C40.8913 60.202 45.9331 68.7017 45.4122 71.2019C44.8913 73.702 31.8913 78.2019 30.9122 78.2019Z"
+						stroke="#283671"
+						stroke-linecap="round"
+					/>
+				</svg>
+				<p class="flex flex-col justify-center">Hey there, I’m Larrisa!</p>
+			</div>
+			<p>
+				I am a data driven designer with an interdisciplinary background in development, design, and
+				storytelling which helps me solve real world problems. <br /> <br /> I’ve spent most of my
+				time singing with The Touchtones and building with the Cornell community to create a
+				platforms geared toward students. <br /> <br /> I have an in-depth understanding of the product
+				cycle and technical expertise which allows me to collaborate effectively across roles. As a data
+				scientist and product designer with experience in Disney's large-scale design systems, I thrive
+				in environments in which I can enrich lives through empathetic, accessible design.
+			</p>
+		</div>
+
+		<img src={headshot} alt="headshot" class="object-cover max-w-[20rem] rounded-[1rem]" />
+	</div>
+
+	<div class="flex max-w-[70%] justify-between align-start">
+		<div class="max-w-[35rem] flex-strink-0">
+			<div>
+				<img alt="recording album" />
+				<svg></svg>
+				<p></p>
+			</div>
+
+			<div>
+				<img alt="acapella concert" />
+				<svg></svg>
+				<p></p>
+			</div>
+
+			<div>
+				<img alt="rainer roadtrip" />
+				<svg></svg>
+				<p></p>
+			</div>
+
+			<div>
+				<img alt="adirondacks rock climbing" />
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="66"
+					height="116"
+					viewBox="0 0 66 116"
+					fill="none"
+				>
+					<path
+						d="M0.5 0.947266C0.223858 0.947266 0 1.17112 0 1.44727C0 1.72341 0.223858 1.94727 0.5 1.94727L0.5 0.947266ZM34.1702 111.071C33.9627 111.253 33.942 111.569 34.1242 111.777L37.0923 115.159C37.2745 115.367 37.5904 115.387 37.7979 115.205C38.0055 115.023 38.0261 114.707 37.8439 114.5L35.2056 111.493L38.2121 108.855C38.4197 108.673 38.4403 108.357 38.2582 108.149C38.076 107.942 37.7601 107.921 37.5525 108.103L34.1702 111.071ZM0.5 1.94727C26.8672 1.94727 42.9562 11.2663 52.4725 24.1812C62.0088 37.1233 65 53.7511 65 68.4471H66C66 53.6429 62.9912 36.7708 53.2775 23.588C43.5438 10.378 27.1328 0.947266 0.5 0.947266L0.5 1.94727ZM65 68.4471C65 83.177 62.9977 94.1732 58.2101 101.293C53.4599 108.358 45.9057 111.69 34.5325 110.948L34.4675 111.946C46.0943 112.704 54.0401 109.287 59.0399 101.851C64.0023 94.4712 66 83.2174 66 68.4471H65Z"
+						fill="#283671"
+						fill-opacity="0.64"
+					/>
+				</svg>
+				<p></p>
+			</div>
+		</div>
+		<p>
+			When I’m not pushing pixels, I love to rock climb, make music, and go on roadtrips. I enjoy
+			watching Survivor and listening to podcasts (pls give me recs!), and I could talk for hours
+			about agricultural ethics, vanilla lattes, and poetry. Thanks for stopping by!
+		</p>
+	</div>
+  <div></div>
+</section>
