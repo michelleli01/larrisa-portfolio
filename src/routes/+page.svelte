64f3cca77b55abe39fa42e9f9b298957f3786ada
Michelle Li <michelleyli123@gmail.com>
2023-12-14 03:40:29 -0500

    add background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import home_pic_1 from '$lib/assets/home_pic_1.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="portfolio home page" />
+</svelte:head>
+
+<section class="py-12">
+	<div class="flex justify-between items-center max-w-[70%] mx-auto">
+		<!-- display: flex;
+flex-direction: column;
+align-items: flex-start;
+gap: 1.25rem; -->
+    <div class="flex flex-col align-start max-w-[30rem] max-h-[7rem] gap-[1.25rem]">
+			<p>Howdy! I’m Larrisa!</p>
+			<p>
+				I’m an designer, story teller, and animal lover currently making magic at Disney✨ and
+				studying at Cornell 🍎.
+			</p>
+		</div>
+
+		<img src={home_pic_1} alt="larrisa on horse" class="object-cover no-repeat max-h-[20vh] rounded-[1rem]" />
+	</div>
+</section>
+
+<section class="bg-portfolio h-screen">
+</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			backgroundImage: {
+				'portfolio': "url('/src/lib/assets/background.png')"
+			}
+		}
 	},
 	plugins: []
 };
